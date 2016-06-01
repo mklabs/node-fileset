f4e0d8e06913fe79e9a0583f752dae02e998d2ed
Mickael Daniel <daniel.mickael@gmail.com>
2016-06-01 16:55:42 +0200

    fix: update old tests to use mocha instead

--- test/mocha.js
+++ test/mocha.js
@@ -21,3 +21,140 @@ describe('Sync API - Given a *.md and **.js pattern, and two exclude', function(
     assert.equal(results.length, 7, 'actually, should return only 7');
   });
 });
+
+// Given a **.md pattern
+describe('Given a **.md pattern', function() {
+  it('returns the list of matching file in this repo', function(done) {
+    fileset('*.md', function(err, results) {
+      if(err) return done(err);
+      assert.ok(Array.isArray(results), 'should be an array');
+      assert.ok(results.length, 'should return at least one element');
+      assert.equal(results.length, 2, 'actually, should return only two');
+      done();
+    });
+  });
+});
+
+describe('Say we want the **.js files, but not those in node_modules', function() {
+  it('recursively walks the dir and returns the matching list', function(done) {
+    fileset('**/*.js', '', function(err, results) {
+      if(err) return done(err);
+      assert.ok(Array.isArray(results), 'should be an array');
+      assert.equal(results.length, 11);
+      done();
+    });
+  });
+
+  it('recursively walks the dir and returns the matching list', function(done) {
+    fileset('**/*.js', function(err, results) {
+      if(err) return done(err);
+      assert.ok(Array.isArray(results), 'should be an array');
+      assert.equal(results.length, 11);
+      done();
+    });
+  });
+
+  it('supports multiple paths at once', function(done) {
+    fileset('**/*.js *.md', 'node_modules/**', function(err, results) {
+      if(err) return done(err);
+
+      assert.ok(Array.isArray(results), 'should be an array');
+      assert.equal(results.length, 13);
+
+      assert.deepEqual(results, [
+        'CHANGELOG.md',
+        'README.md',
+        'lib/fileset.js',
+        'test/fixtures/an (odd) filename.js',
+        'test/fixtures/glob/common.js',
+        'test/fixtures/glob/glob.js',
+        'test/fixtures/glob/sync.js',
+        'test/fixtures/minimatch/minimatch.js',
+        'test/mocha.js',
+        'tests/fixtures/an (odd) filename.js',
+        'tests/helper.js',
+        'tests/test-sync.js',
+        'tests/test.js'
+      ]);
+
+      done();
+    });
+  });
+
+  it('Should support multiple paths for excludes as well', function(done) {
+    fileset('**/*.js *.md', 'node_modules/** **.md tests/*.js', function(err, results) {
+      if(err) return done(err);
+      assert.ok(Array.isArray(results), 'should be an array');
+      assert.equal(results.length, 8);
+
+      assert.deepEqual(results, [
+        'lib/fileset.js',
+        'test/fixtures/an (odd) filename.js',
+        'test/fixtures/glob/common.js',
+        'test/fixtures/glob/glob.js',
+        'test/fixtures/glob/sync.js',
+        'test/fixtures/minimatch/minimatch.js',
+        'test/mocha.js',
+        'tests/fixtures/an (odd) filename.js',
+      ]);
+
+      done();
+    });
+  });
+});
+
+describe('Testing out emmited events', function() {
+  it('recursively walk the dir and return the matching list', function(done) {
+    fileset('**/*.js', 'node_modules/**')
+      .on('error', done)
+      .on('end', function(results) {
+        assert.ok(Array.isArray(results), 'should be an array');
+        assert.equal(results.length, 11);
+        done();
+      });
+  });
+
+  it('support multiple paths at once', function(done) {
+    fileset('**/*.js *.md', 'node_modules/**')
+      .on('error', done)
+      .on('end', function(results) {
+        assert.ok(Array.isArray(results), 'should be an array');
+        assert.equal(results.length, 13);
+
+        assert.deepEqual(results, [
+          'CHANGELOG.md',
+          'README.md',
+          'lib/fileset.js',
+          'test/fixtures/an (odd) filename.js',
+          'test/fixtures/glob/common.js',
+          'test/fixtures/glob/glob.js',
+          'test/fixtures/glob/sync.js',
+          'test/fixtures/minimatch/minimatch.js',
+          'test/mocha.js',
+          'tests/fixtures/an (odd) filename.js',
+          'tests/helper.js',
+          'tests/test-sync.js',
+          'tests/test.js'
+        ]);
+
+        done();
+      });
+  });
+});
+
+describe('Testing patterns passed as arrays', function() {
+  it('match files passed as an array with odd filenames', function(done) {
+    fileset(['lib/*.js', 'test/fixtures/an (odd) filename.js'], ['node_modules/**'])
+      .on('error', done)
+      .on('end', function(results) {
+        assert.ok(Array.isArray(results), 'should be an array');
+        assert.equal(results.length, 2);
+        assert.deepEqual(results, [
+          'lib/fileset.js',
+          'test/fixtures/an (odd) filename.js',
+        ]);
+
+        done();
+      });
+  });
+});
